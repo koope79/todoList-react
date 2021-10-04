5e861fcebada01127200ad11e1527b30222d6e51
koope79 <70471003+koope79@users.noreply.github.com>
2021-10-05 00:02:18 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,10 +15,12 @@ class HeaderC extends React.Component {
     }
 
     sendNote = () => {
-        if(this.state.inputValue.length > 0) this.props.addNote(this.state.inputValue);
-        this.setState({
-            inputValue: ""
-        });
+        if (this.state.inputValue.length > 0) {
+            this.props.addNote(this.state.inputValue);
+            this.setState({
+                inputValue: ""
+            });
+        }
     }
 
     render() {
@@ -27,7 +29,7 @@ class HeaderC extends React.Component {
                 <h2>Todo List</h2>
                 <div className={s.input}>
                     <input onChange={this.onChangeVal} value={this.state.inputValue} placeholder="Start to write..."></input>
-                    <button onClick={this.sendNote}><i className="fa fa-plus"></i></button>
+                    <button onClick={this.sendNote}><i className={"fa fa-plus"}></i></button>
                 </div>
             </div>
         );
